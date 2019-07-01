e9198d9d0887cf2c8c50d43f6a29e54b5a3fdf69
liyali <liyali@qq.com>
2019-07-01 14:26:21 +0800

    项目初始化

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 2. 删除 项目中的src目录
 3. 新建一个src 目录 在里面创建assets文件夹里面放置一些静态文件  创建store目录 放置数据  创建main.js入口文件  创建router.js 路由文件 创建views文件夹 放置页面文件
 
+##cssBEM
+
+##hotcss
+先下载 
---
+++ src/components/Tabbar/index.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="mz-tabbar">
+        <ul class="mz-tabbar__ul">
+            <li class="mz-tabbar__li" v-for="(tab,index) in tabs" :key="index">
+                <router-link :to="tab.to" active-class="active">
+                <i class="iconfont" :class="tab.icon" ></i>
+                <span>{{ tab.name }}</span>
+                </router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Tabbar',
+
+    props: {
+        //
+        tabs: {
+            type: Array,
+            default() {
+                return [];
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import '../../assets/styles/common/px2rem.scss';
+@import '../../assets/styles/common/mixins.scss';
+    .mz-tabbar {
+        @include border-top;
+        @include border-bottom;
+        height: px2rem(50);
+        background: #fff;
+        border-top: 1px solid #ededed;
+        border-bottom:1px solid #ededed;
+
+        &__ul {
+            display: flex;
+            height: 100%;
+        }
+
+        &__li {
+            flex: 1;
+
+            a {
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                flex-direction: column;
+                align-items: center;
+                text-decoration: none;
+                font-size: 14px;
+                color: #797d82;
+                .iconfont {
+                    font-size: 40px;
+                    margin-bottom: 4px;
+                }
+
+                &.active {
+                    color: #ff5f16;
+                }
+            }
+        }
+    }
+</style>
+
